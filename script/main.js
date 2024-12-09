86b8f1b921133e7af202b3be525638022e49b0b9
Mitchel Mathias <mitchelmathias2904@gmail.com>
2024-12-09 17:03:30 -0300

    Mesma coisa

--- script/main.js
+++ script/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
+    // Verifica se a carrousel já foi inicializada antes
+    if ($('.carrousel').hasClass('slick-initialized')) {
+        $('.carrousel').slick('unslick');
+    }
+    
     $('.carrousel').slick({
-        slidesToShow: 5, 
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [
             {
-                breakpoint: 1024, 
+                breakpoint: 650, 
                 settings: {
                     slidesToShow: 1,  
                     slidesToScroll: 1,
