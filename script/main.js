a2fb2306a70eed71f670176ccd468ef3163326b6
Mitchel Mathias <mitchelmathias2904@gmail.com>
2024-12-09 16:40:28 -0300

    Anexando Comentários

---
+++ script/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.carrousel').slick({
+        autoplay:true,
+        infinity: true,
+        autoplaySpeed: 1000,
+        centerMode: true,
+        arrows:false,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
